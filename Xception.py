a4e56ab0ddf6912a67cbd05420cfe377b07ddcd1
Nandha Kumar G <102609946+nandhakumar1301@users.noreply.github.com>
2023-10-02 17:41:13 +0530

    Add files via upload

---
+++ Xception.py
@@ -0,0 +1,350 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Importing Required Libraries
+
+# ## Extracting Files
+
+# In[ ]:
+
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('darkgrid')
+
+import os
+import random
+import keras
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.model_selection import train_test_split
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+
+from sklearn.metrics import classification_report
+import tensorflow as tf
+
+
+# In[ ]:
+
+
+c = '../input/covid19-radiography-database/COVID-19_Radiography_Dataset/COVID/images'
+n = '../input/covid19-radiography-database/COVID-19_Radiography_Dataset/Normal/images'
+p = '../input/covid19-radiography-database/COVID-19_Radiography_Dataset/Viral Pneumonia/images'
+
+
+# In[ ]:
+
+
+random.seed(42)
+filenames = os.listdir(c) + random.sample(os.listdir(n), 2500) + os.listdir(p)
+
+
+# ### Classes
+# * 0 -> Normal
+# * 1 -> Viral Pneumonia
+# * 2 -> COVID
+
+# In[ ]:
+
+
+categories = []
+for filename in filenames:
+    category = filename.split('-')[0]
+    if category == 'COVID':
+        categories.append(str(2))
+    elif category == 'Viral Pneumonia':
+        categories.append(str(1))
+    else:
+        categories.append(str(0))
+
+
+# ## Converting to Dataframe
+
+# In[ ]:
+
+
+for i in range(len(filenames)):
+    if 'COVID' in filenames[i]:
+        filenames[i] = os.path.join(c, filenames[i])
+    elif 'Viral Pneumonia' in filenames[i]:
+        filenames[i] = os.path.join(p, filenames[i])
+    else:
+        filenames[i] = os.path.join(n, filenames[i])
+    
+    
+df = pd.DataFrame({
+    'filename': filenames,
+    'category': categories
+})
+
+
+# In[ ]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+df.shape
+
+
+# ## Exploratory Data Analysis
+
+# In[ ]:
+
+
+plt.figure(figsize=(12,3))
+splot = sns.countplot(data = df.sort_values(by='category'), y ='category', palette='cool', saturation=0.9)
+plt.bar_label(container=splot.containers[0], 
+              labels=['Normal', 'Viral Pneumonia', 'COVID'], 
+              label_type='center', size=15, color='w')
+plt.show()
+
+
+# ## Data Visualization
+
+# In[ ]:
+
+
+sample = random.choice(df['filename'])
+image = load_img(sample)
+plt.imshow(image)
+plt.show()
+
+
+# ## Splitting Train, Validation & Test Data
+
+# In[ ]:
+
+
+train_data, test_valid_data = train_test_split(df, test_size=0.2, random_state = 42, shuffle=True, stratify=df['category'])
+train_data = train_data.reset_index(drop=True)
+test_valid_data = test_valid_data.reset_index(drop=True)
+
+
+# In[ ]:
+
+
+test_data, valid_data = train_test_split(test_valid_data, test_size=0.5, random_state = 42,
+                                         shuffle=True, stratify=test_valid_data['category'])
+test_data = test_data.reset_index(drop=True)
+valid_data = valid_data.reset_index(drop=True)
+
+
+# ## Data Augmentation
+
+# In[ ]:
+
+
+train_data_gen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+
+# In[ ]:
+
+
+train_generator = train_data_gen.flow_from_dataframe(
+    train_data, 
+    x_col='filename',
+    y_col='category',
+    target_size=(224,224),
+    class_mode='categorical',
+    batch_size=32
+)
+
+
+# In[ ]:
+
+
+valid_data_gen = ImageDataGenerator(rescale=1./255)
+
+valid_generator = valid_data_gen.flow_from_dataframe(
+    valid_data, 
+    x_col='filename',
+    y_col='category',
+    target_size=(224,224),
+    class_mode='categorical',
+    batch_size=32
+)
+
+
+# ## Model Training - Xception
+
+# #### Model Architecture
+
+# In[ ]:
+
+
+engine = tf.keras.applications.Xception(
+        # Freezing the weights of the top layer in the InceptionResNetV2 pre-traiined model
+        include_top = False,
+
+        # Use Imagenet weights
+        weights = 'imagenet',
+
+        # Define input shape to 224x224x3
+        input_shape = (224 , 224 , 3),
+
+    )
+    
+x = tf.keras.layers.GlobalAveragePooling2D(name = 'avg_pool')(engine.output)
+x =tf.keras.layers.Dropout(0.75)(x)
+x = tf.keras.layers.BatchNormalization(
+                      axis=-1,
+                      momentum=0.99,
+                      epsilon=0.01,
+                      center=True,
+                      scale=True,
+                      beta_initializer="zeros",
+                      gamma_initializer="ones",
+                      moving_mean_initializer="zeros",
+                      moving_variance_initializer="ones",
+                  )(x)
+out = tf.keras.layers.Dense(3, activation = 'softmax', name = 'dense_output')(x)
+
+
+    # Build the Keras model
+model = tf.keras.models.Model(inputs = engine.input, outputs = out)
+    # Compile the model
+
+model.compile(
+        # Set optimizer to Adam(0.0001)
+        optimizer = tf.keras.optimizers.Adam(learning_rate= 3e-4),
+        #optimizer= SGD(lr=0.001, decay=1e-6, momentum=0.99, nesterov=True),
+        # Set loss to binary crossentropy
+        #loss = tf.keras.losses.SparseCategoricalCrossentropy(),
+        loss = 'categorical_crossentropy',
+        # Set metrics to accuracy
+        metrics = ['accuracy']
+    )
+
+
+# #### Model Summary
+
+# In[ ]:
+
+
+model.summary()
+
+
+# In[ ]:
+
+
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+learning_rate_reduction = keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy',
+                                                            patience=2,
+                                                            verbose=2,
+                                                            factor=0.5,
+                                                            min_lr=0.00001)
+reduce_lr =  keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                              patience=3, min_lr=0.00001)
+
+
+# In[ ]:
+
+
+history = model.fit(train_generator,
+                             validation_data=valid_generator, verbose=1, epochs=15,
+                      callbacks=[early_stopping, reduce_lr , learning_rate_reduction]
+                       )
+
+
+# # Final Test
+
+# In[ ]:
+
+
+test_set = valid_data_gen.flow_from_dataframe(
+    test_data, 
+    x_col='filename',
+    y_col='category',
+    target_size=(224,224),
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=False
+)
+
+
+# In[ ]:
+
+
+model.evaluate(test_set)
+
+
+# In[ ]:
+
+
+prob = model.predict(test_set)
+predIdxs = np.argmax(prob, axis=1) 
+
+
+print('\n')
+print(classification_report(test_set.labels, predIdxs,target_names = ['Normal','Viral Pneumonia', 'COVID'], digits=2))
+
+
+# ## Saving Model
+
+# In[ ]:
+
+
+from sklearn.metrics import confusion_matrix
+confusion_matrix(test_set.labels,predIdxs )
+
+
+# In[ ]:
+
+
+plt.figure(figsize=(10,6))
+import seaborn as sns
+fx= sns.heatmap(confusion_matrix(test_set.labels,predIdxs), annot=True, fmt=".2f",cmap="GnBu")
+fx.set_title('Confusion Matrix \n');
+fx.set_xlabel('\n Predicted Values\n')
+fx.set_ylabel('Actual Values\n');
+fx.xaxis.set_ticklabels(['NORMAL','PNEUMONIA','COVID-19'])
+fx.yaxis.set_ticklabels(['NORMAL','PNEUMONIA','COVID-19'])
+plt.show()
+
+
+# In[ ]:
+
+
+model.save('Covid.h5')
+
+
+# In[ ]:
+
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='lower right')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('train set loss')
+
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper right')
+plt.show()
+
